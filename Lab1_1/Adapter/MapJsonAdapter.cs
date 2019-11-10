5e7f5e092c8ce571c445e0730661908ad6fd74f6
evkus <evkus@DESKTOP-1T0FVO2>
2019-11-11 01:01:59 +0200

    Implemented Adapter pattern

---
+++ Lab1_1/Adapter/MapJsonAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_1.Adapter
+{
+    public class MapJsonAdapter
+    {
+        MapJson map;
+        public List<Unit> ConvertJsonToList()
+        {
+            return map.ConvertArrayToList();
+        }
+        public void ConvertListToJson(List<Unit> unitList)
+        {
+            map.ConvertListToArray(unitList);
+        }
+    }
+}
